5e80011ab302f7f6621c5f0a501cf4101a1dcaca
Ibrahim . Aliu <ibrahim.aliu@musala.com>
2021-02-18 16:26:56 +0100

    removed unused colors file

--- src/utilities/colors.js
+++
@@ -1,9 +0,0 @@
-export const colors = {
-    'black': '#3F403F',
-    'green': '#475841',
-    'blue': '#9FB8AD',
-    'gray': '#CED0CE',
-    'platinum': '#E6E8E6',
-    'violet': '#58355E',
-    'red': '#ec4c4c'
-}
